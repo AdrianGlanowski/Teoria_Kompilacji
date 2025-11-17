98f9126685b5b04143d061807cb4aaac270610fe
AdrianGlanowski <adrianglanowski04@gmail.com>
2025-11-17 11:58:17 +0100

    custom run implementation

--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ import sys
 from scanner import Scanner
 from parser import Mparser
 
+def main():
+        lexer = Scanner()
+        parser = Mparser()
+
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example1.m"
+        with open(f"src/examples/{filename}", "r") as file:
+            text = file.read()
+
+        parser.parse(lexer.tokenize(text))
+
 
 if __name__ == '__main__':
-
-    lexer = Scanner()
-    parser = Mparser()
-
-    filename = sys.argv[1] if len(sys.argv) > 1 else "src/examples/example1.m"
-    with open(filename, "r") as file:
-        text = file.read()
-
-    parser.parse(lexer.tokenize(text))
+    main()
