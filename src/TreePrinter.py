b870da00f2cc64e03a8e38b29c4f5cd9f7d03cde
radek <radek.rogalski@gmail.com>
2025-11-21 14:37:12 +0100

    cleanup

--- src/TreePrinter.py
+++ src/TreePrinter.py
@@ -11,9 +11,7 @@ class TreePrinter:
     def safePrintTree(obj, indent):
         """Helper function to handle primitives and objects with `printTree`."""
         prefix = "|  " * indent
-        if isinstance(obj, (int, float, str)):
-            print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{prefix}{obj}")
-        elif obj is not None:
+        if obj is not None:
             obj.printTree(indent)
         else:
             print(f"{prefix}None")
